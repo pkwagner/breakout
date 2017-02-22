9d273fea83aaed1e6c4ffec22e6145a36cf81b3c
games647 <games@tsprivat.de>
2017-02-22 16:19:20 +0100

    Add pause implementation (Fixes #7)
    Add a list of available keyboard shortcuts
    
    Introduce a extra method for borders
    
    Add more javadoc
    
    Add logging call

--- ReadMe.md
+++ ReadMe.md
@@ -10,7 +10,8 @@ This is a reimplementation of the game Breakout using LWJGL and a given template
 
 * Stick movement
 * Ball movement
-* Ball collides with other entities
+* Ball collides with other entities (i.e. Border and Stick)
+* Pause game on escape
 
 ## Setup
 
@@ -18,3 +19,11 @@ This is a reimplementation of the game Breakout using LWJGL and a given template
 2. Include the eea.jar, slick.jar and all jars in log4j/ and lwjgl/jar
 3. Set the Breakout class as the main class
 4. Add a "-Djava.library.path=ABSOLUTE_PATH_TO_LWGL_NATIVE" as vm startup argument
+
+## Available keyboard shortcuts
+
+| State | Input | Effect |
+| --- | ---|---|
+| Gameplay | Left Arrow | Moves the stick left |
+| Gameplay | Right Arrow | Moves the stick right |
+| Gameplay | Escape | Pauses the game |
--- src/de/tudarmstadt/informatik/fop/breakout/states/GameplayState.java
+++ src/de/tudarmstadt/informatik/fop/breakout/states/GameplayState.java
@@ -1,5 +1,6 @@
 package de.tudarmstadt.informatik.fop.breakout.states;
 
+import de.tudarmstadt.informatik.fop.breakout.actions.PauseToggleAction;
 import de.tudarmstadt.informatik.fop.breakout.constants.GameParameters;
 import de.tudarmstadt.informatik.fop.breakout.controllers.BallController;
 import de.tudarmstadt.informatik.fop.breakout.controllers.ClockController;
@@ -14,12 +15,17 @@ import de.tudarmstadt.informatik.fop.breakout.views.BallRenderComponent;
 import de.tudarmstadt.informatik.fop.breakout.views.ClockRenderComponent;
 import de.tudarmstadt.informatik.fop.breakout.views.StickRenderComponent;
 
+import eea.engine.component.render.ImageRenderComponent;
 import eea.engine.entity.Entity;
 import eea.engine.entity.StateBasedEntityManager;
+import eea.engine.event.basicevents.KeyPressedEvent;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -54,11 +60,6 @@ public class GameplayState extends BasicGameState {
         ClockRenderComponent clockView = new ClockRenderComponent("clock_view");
         clockModel.addComponent(clockView);
         
-        //borders
-        Entity leftBorder = new BorderFactory(GameParameters.BorderType.LEFT).createEntity();
-        Entity rightBorder = new BorderFactory(GameParameters.BorderType.RIGHT).createEntity();
-        Entity topBorder = new BorderFactory(GameParameters.BorderType.TOP).createEntity();
-
         ballController.init(stateBasedGame);
         stickController.init(stateBasedGame);
         clockController.init(stateBasedGame);
@@ -66,11 +67,12 @@ public class GameplayState extends BasicGameState {
 
         addEntity(stickModel);
         addEntity(ballModel);
-        addEntity(leftBorder);
-        addEntity(rightBorder);
-        addEntity(topBorder);
 
+        addBorders(gameContainer);
+        addPauseEntity(gameContainer);
+        
         MapController mapController = new MapController(gameContainer, this);
+
         mapController.loadMap();
         
         addEntity(clockModel);
@@ -99,5 +101,44 @@ public class GameplayState extends BasicGameState {
     @Override
     public int getID() {
         return stateId;
+    }
+
+    private void addBorders(GameContainer gameContainer) {
+        Entity leftBorder = new BorderFactory(GameParameters.BorderType.LEFT).createEntity();
+        Entity rightBorder = new BorderFactory(GameParameters.BorderType.RIGHT).createEntity();
+        Entity topBorder = new BorderFactory(GameParameters.BorderType.TOP).createEntity();
+
+        addEntity(leftBorder);
+        addEntity(rightBorder);
+        addEntity(topBorder);
+    }
+
+    /**
+     * Adds an entity for pausing the game.
+     * <p>
+     * It will toggle the pause state on keyboard input and will show up an pause-image on pause.
+     *
+     * @param gameContainer game instance container
+     * @throws SlickException if the pause cannot be found
+     */
+    private void addPauseEntity(GameContainer gameContainer) throws SlickException {
+        Entity pauseEntity = new Entity(GameParameters.PAUSE_ID);
+
+        //default hides the entity and make it passable so it won't effect the gameplay
+        pauseEntity.setVisible(false);
+        pauseEntity.setPassable(true);
+
+        //center the entity
+        pauseEntity.setPosition(new Vector2f(gameContainer.getWidth() / 2, gameContainer.getHeight() / 2));
+
+        //view component
+        pauseEntity.addComponent(new ImageRenderComponent(new Image(GameParameters.PAUSE_IMAGE)));
+
+        //key listener
+        KeyPressedEvent escapeKeyEvent = new KeyPressedEvent(Input.KEY_ESCAPE);
+        escapeKeyEvent.addAction(new PauseToggleAction());
+        pauseEntity.addComponent(escapeKeyEvent);
+
+        addEntity(pauseEntity);
     }
 }
