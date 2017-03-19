6d393dabd80caf639392f6ba985d5103dbf17fd6
Paul Konstantin Wagner <pw76kate@rbg.informatik.tu-darmstadt.de>
2017-03-19 15:34:35 +0100

    Fixed smash ball item

--- src/de/tudarmstadt/informatik/fop/breakout/actions/items/SmashBallItemAction.java
+++ src/de/tudarmstadt/informatik/fop/breakout/actions/items/SmashBallItemAction.java
@@ -2,26 +2,28 @@ package de.tudarmstadt.informatik.fop.breakout.actions.items;
 
 import de.tudarmstadt.informatik.fop.breakout.constants.GameParameters;
 import de.tudarmstadt.informatik.fop.breakout.models.BallModel;
+import de.tudarmstadt.informatik.fop.breakout.states.GameplayState;
 import eea.engine.entity.StateBasedEntityManager;
 import org.newdawn.slick.state.StateBasedGame;
 
+import java.util.ArrayList;
+
 public class SmashBallItemAction extends AbstractItemAction {
 
-    private BallModel ball;
+    private ArrayList<BallModel> allBalls;
 
     @Override
     protected void init(StateBasedGame stateBasedGame) {
-        ball = (BallModel) StateBasedEntityManager.getInstance().getEntity(GameParameters.GAMEPLAY_STATE, GameParameters.BALL_ID);
+        allBalls = ((GameplayState) stateBasedGame.getState(GameParameters.GAMEPLAY_STATE)).getBalls();
     }
 
     @Override
     public void onEnable() {
-        ball.setSmashMode(true);
+        allBalls.forEach(ball -> ball.setSmashMode(true));
     }
 
     @Override
     public void onDisable() {
-        // TODO Check if ball is inside a block
-        ball.setSmashMode(false);
+        allBalls.forEach(ball -> ball.setSmashMode(false));
     }
 }
